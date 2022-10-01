ed6cd3364cb390d7135cd19faa81aff0e7dbeffd
Ricardo Gatgens <gatgens48@gmail.com>
2022-10-01 16:52:42 -0600

    navbar

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Components/home/home.component';
-import { FormsModule } from '@angular/forms';
-import { MakeAppointmentComponent } from './Components/make-appointment/make-appointment.component';
-import { BillsComponent } from './Components/bills/bills.component';
 
 import { LoginFormComponent } from './Components/login-form/login-form.component';
 import { MessageComponent } from './Components/message/message.component';
 
+import { AdminMainComponent } from './Components/admin-main/admin-main.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 import { EmployeesComponent } from './Components/employees/employees.component';
 import { BranchesComponent } from './Components/branches/branches.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
-import { AdminMainComponent } from './Components/admin-main/admin-main.component';
+
+import { HomeComponent } from './Components/home/home.component';
+import { MakeAppointmentComponent } from './Components/make-appointment/make-appointment.component';
+import { BillsComponent } from './Components/bills/bills.component';
 
 @NgModule({
   declarations: [
