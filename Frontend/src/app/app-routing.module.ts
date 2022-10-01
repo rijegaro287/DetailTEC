0d053d74599135d01b8f1f8975b2c9054b7e899b
Yorbre25 <yraulbr25@gmail.com>
2022-10-01 15:14:45 -0600

    bases de home y registro de citas

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { MakeAppointmentComponent } from './Components/make-appointment/make-appointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '',
+  // component: HomeComponent
+  component: MakeAppointmentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { MakeAppointmentComponent } from './Components/make-appointment/make-appointment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MakeAppointmentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
