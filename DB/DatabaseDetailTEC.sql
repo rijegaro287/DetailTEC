2e153a85fdaa458d60864bce5c2747181dccccde
adri <62965634+cuadriante@users.noreply.github.com>
2022-10-19 17:47:05 -0600

    cambios
    cambios

--- DB/DatabaseDetailTEC.sql
+++ DB/DatabaseDetailTEC.sql
@@ -1,7 +1,4 @@
-use master
-go
-if not exists (SELECT name FROM master.dbo.sysdatabases WHERE NAME = 'DetailTEC')
-create database DetailTEC
+-- create database DetailTEC
 
 go 
 
@@ -9,7 +6,7 @@ use DetailTEC
 
 go
 
-if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TRABAJADOR')
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TRABAJADOR')
 create table TRABAJADOR(
 	Cedula char(9) not null,
 	Nombre varchar(20) not null,
@@ -17,43 +14,46 @@ create table TRABAJADOR(
 	Apellido2 varchar(20),
 	Fecha_nacimiento date,
 	Fecha_ingreso date,
-	Edad tinyint,
+	Edad tinyint, --La edad se calcula en la base de datos, no se ingresa
 	PasswordT varchar(20) not null,
 	Rol varchar(20) not null,
-	Tipo_pago varchar(10) not null,
+	Tipo_pago varchar(10) not null, -- El tipo de pago debería ser otra tabla
+	-- El trabajador debería de tener una sucursal asignada
 	PRIMARY KEY(Cedula)
 )
 
-go
 
-if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TRABAJADOR')
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'SUCURSAL')
 create table SUCURSAL(
 
-	Nombre varchar(20) not null,
+	Nombre varchar(40) not null,
 	Provincia varchar(20),
 	Canton varchar(20),
 	Distrito varchar(20),
 	Telefono char(8) not null,
 	Fecha_apertura date,
-	Fecha_gerente date,
-	Cedula_gerente char(9) not null,
+	Fecha_gerente date, --Debería ser otra tabla
+	Cedula_gerente char(9) not null, -- Va con la tabla de arriba
 	PRIMARY KEY(Nombre),
 	FOREIGN KEY(Cedula_gerente) REFERENCES TRABAJADOR(Cedula)
 
 )
 
-
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'LAVADO')
 CREATE TABLE LAVADO
 (
-	Nombre varchar(20) not null,
+	Nombre varchar(40) not null,
 	Costo int not null,
 	Precio int not null,
 	Duracion int
+	-- FALTAN LOS PRODUCTOS UTILIZADOS
+	-- FALTA EL PERSONAL REQUERIDO
+	-- FALTAN LOS PUNTOS QUE SE OBTIENEN
 	PRIMARY KEY (Nombre) 
 )
 
 
-
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'CLIENTE')
 create table CLIENTE(
 
 	Cedula char(9) not null,
@@ -63,13 +63,13 @@ create table CLIENTE(
 	Usuario varchar(20) not null,
 	Correo varchar(50) not null,
 	PasswordC varchar(20) not null,
-	Puntos int not null,
+	Puntos int not null, --Al crear el usuario se le asignan 0 puntos
 	PRIMARY KEY(Cedula)
 
 )
 
 
-
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TELEFONOS_CLIENTE')
 create table TELEFONOS_CLIENTE(
 	
 	Cedula_Cli char(9) not null,
@@ -78,6 +78,7 @@ create table TELEFONOS_CLIENTE(
 	
 )
 
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'DIRECCIONES_CLIENTE')
 create table DIRECCIONES_CLIENTE(
 	
 	Cedula_Cli char(9) not null,
@@ -86,23 +87,26 @@ create table DIRECCIONES_CLIENTE(
 	
 )
 
-
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'CITA')
 create table CITA(
 
-	ID int not null,
+	ID int not null, -- el id se debería de generar automáticamente, no tener que ingresarlo
 	Placa char(6) not null,
-	Nombre_sucursal varchar(20) not null,
-	Nombre_lavado varchar(20) not null,
+	Nombre_sucursal varchar(40) not null,
+	Nombre_lavado varchar(40) not null,
 	Cedula_cliente char(9) not null,
+	--Falta fecha
+	Atendido_por char(9) not null, --Esto lo agregué de forma temporal, para probar el report
 	PRIMARY KEY(ID),
 	FOREIGN KEY(Nombre_sucursal) REFERENCES SUCURSAL(Nombre),
 	FOREIGN KEY(Nombre_lavado) REFERENCES LAVADO(Nombre),
 	FOREIGN KEY(Cedula_cliente) REFERENCES CLIENTE(Cedula),
+	Foreign Key(Atendido_por) REFERENCES TRABAJADOR(Cedula)
 
 )
 
 
-
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TRABAJADORES_POR_CITA')
 create table TRABAJADORES_POR_CITA(
 	
 	Cedula_trabajador char(9) not null,
@@ -112,7 +116,7 @@ create table TRABAJADORES_POR_CITA(
 )
 
 
-
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'PROVEEDOR')
 CREATE TABLE PROVEEDOR 
 (
 	Cedula_juridica char(9) not null,
@@ -122,6 +126,7 @@ CREATE TABLE PROVEEDOR
 	PRIMARY KEY (Cedula_juridica)
 );
 
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'CONTACTO_PROVEEDOR')
 CREATE TABLE CONTACTO_PROVEEDOR
 (
 	Ced_prov char(9) not null,
@@ -129,6 +134,7 @@ CREATE TABLE CONTACTO_PROVEEDOR
 	FOREIGN KEY(Ced_prov) REFERENCES PROVEEDOR(Cedula_juridica) 
 );
 
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'PRODUCTO')
 CREATE TABLE PRODUCTO
 (
 	Nombre varchar(20) not null,
@@ -139,6 +145,7 @@ CREATE TABLE PRODUCTO
 	FOREIGN KEY (Ced_prov) REFERENCES PROVEEDOR(Cedula_juridica)
 ); 
 
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'PRODUCTOS_POR_CLIENTE')
 CREATE TABLE PRODUCTOS_POR_CLIENTE
 (
 	Cedula_cliente char(9) not null,
@@ -147,15 +154,32 @@ CREATE TABLE PRODUCTOS_POR_CLIENTE
 	FOREIGN KEY (Nombre_producto) REFERENCES PRODUCTO(Nombre)
 ); 
 
-
+-- if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'PRODUCTOS_POR_SUCURSAL')
 CREATE TABLE PRODUCTOS_POR_SUCURSAL 
 (
 	Nombre_producto varchar(20),
-	Nombre_sucursal varchar(20) 
+	Nombre_sucursal varchar(40) 
 	FOREIGN KEY (Nombre_producto) REFERENCES PRODUCTO(Nombre),
 	FOREIGN KEY (Nombre_sucursal) REFERENCES SUCURSAL(Nombre)
 ); 
 
+CREATE TABLE FACTURA( --Hice esta tabla solo para poder desplegar el reporte de facturas, le faltan muchas cosas
+	ID int not null,
+	medio_pago int not NULL,
+	total int not null,
+	PRIMARY KEY(ID)
+);
+
+CREATE TABLE TIPO_DE_PAGO( -- esta si está bien hecha juas juas juas
+	ID int not null,
+	Nombre varchar(20) not null,
+	PRIMARY KEY (ID)
+);
+
+-- LLaves foráneas 
 
 
-
+ALTER TABLE FACTURA 
+ADD CONSTRAINT FK_FACTURA_TIPO_DE_PAGO FOREIGN KEY (medio_pago) REFERENCES TIPO_DE_PAGO(ID);
+ALTER TABLE FACTURA
+ADD CONSTRAINT FK_FACTURA_CITA FOREIGN KEY (ID) REFERENCES CITA(ID);
